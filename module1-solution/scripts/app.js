28315d70bb896019ef442b86bad80c48cf706d14
mikecoronel <coronel.miguel79@gmail.com>
2016-11-30 00:41:36 -0600

    Almost complete

--- module1-solution/scripts/app.js
+++ module1-solution/scripts/app.js
@@ -1,6 +1,32 @@
 (function (){
+  'use strict';
+
   angular.module('LunchCheck', [])
-  .controller('LunchCheckController', function($scope){
-    $scope.name = "test";
-  })
+  .controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+
+  function LunchCheckController ($scope){
+    $scope.message = "";
+    $scope.checkMyHungry = function () {
+      $scope.customStyle = {};
+      var menuLength = 0;
+
+      if ($scope.myLunch != undefined)
+        menuLength = $scope.myLunch.split(',').length;
+        $scope.customStyle.colorClass = "green";
+
+      switch(true){
+        case menuLength == 0:
+          $scope.message = "Please enter data first";
+          $scope.customStyle.colorClass = "red";
+          break;
+        case menuLength <= 3:
+          $scope.message = "Enjoy!";
+          break;
+        default:
+          $scope.message = "Too much!";
+      }
+    }
+  }
 })();
